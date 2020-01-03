d7f8ea84f5a361a7975e7ba03393ce19e07d14e8
Raul Baldner Junior <rjunior@admatic.com.br>
2020-01-03 16:07:08 -0300

    Lint

--- examples/support/logger.js
+++ examples/support/logger.js
@@ -1,27 +1,38 @@
-"use strict"
+const BgReset = '\x1b[0m'
 
-const BgReset = "\x1b[0m"
-
-const FgRed = "\x1b[31m"
-const FgGreen = "\x1b[32m"
-const FgYellow = "\x1b[33m"
-const FgCyan = "\x1b[36m"
-const FgWhite = "\x1b[39m"
-const FgWhiteBright = "\x1b[97m"
-
+const FgRed = '\x1b[31m'
+const FgYellow = '\x1b[33m'
+const FgCyan = '\x1b[36m'
+const FgWhiteBright = '\x1b[97m'
 
 // logger object
 module.exports = tag => ({
   info(args) {
-    console.log(FgCyan, JSON.stringify(Object.assign({ level: "info", tag }, args)), BgReset)
+    console.log(
+      FgCyan,
+      JSON.stringify({ level: 'info', tag, ...args }),
+      BgReset
+    )
   },
   debug(args) {
-    console.log(FgWhiteBright, JSON.stringify(Object.assign({ level: "debug", tag }, args)), BgReset)
+    console.log(
+      FgWhiteBright,
+      JSON.stringify({ level: 'debug', tag, ...args }),
+      BgReset
+    )
   },
   warn(args) {
-    console.log(FgYellow, JSON.stringify(Object.assign({ level: "warn", tag }, args)), BgReset)
+    console.log(
+      FgYellow,
+      JSON.stringify({ level: 'warn', tag, ...args }),
+      BgReset
+    )
   },
   error(args) {
-    console.log(FgRed, JSON.stringify(Object.assign({ level: "error", tag }, args)), BgReset)
-  },
+    console.log(
+      FgRed,
+      JSON.stringify({ level: 'error', tag, ...args }),
+      BgReset
+    )
+  }
 })
--- test/setup.js
+++ test/setup.js
@@ -1,24 +1,21 @@
-"use strict"
-
-const rabbitTestFactory = require("rabbit-test-helper")
+const rabbitTestFactory = require('rabbit-test-helper')
 
 const config = {
-  url: "amqp://localhost",
+  url: 'amqp://localhost',
   exchange: {
-    name: "app_test",
+    name: 'app_test',
     routingKeys: [
       {
-        name: "clicks",
-        queues: [
-          "clicks_warehouse",
-        ],
-      },
-    ],
-  },
+        name: 'clicks',
+        queues: ['clicks_warehouse']
+      }
+    ]
+  }
 }
 
 // generate a helper to build a rabbit objects
 global.RabbitHelper = rabbitTestFactory(config)
-global.expect = require("chai").expect
+global.expect = require('chai').expect
+
 global.waitSeconds = seconds =>
   new Promise(resolve => setTimeout(resolve, seconds * 1000))
