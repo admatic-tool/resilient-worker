edbb6365cca3580010bf7cdfbdf9e43fe8482094
Raul Baldner Junior <rjunior@admatic.com.br>
2020-01-09 18:48:24 -0300

    Adiciona mais testes ao RabbitBroker.publish

--- test/unit/brokers/rabbit.spec.js
+++ test/unit/brokers/rabbit.spec.js
@@ -92,11 +92,106 @@ describe('RabbitBroker', () => {
     })
   })
 
-  context('.publish', () => {})
+  context('.publish', () => {
+    context('when _getPublisherChannel throws error', () => {
+      it('should not swallow error', function*() {
+        const rabbit = new RabbitBroker({ validate: false })
+
+        sinon.stub(rabbit, '_getPublisherChannel').throws('Test error')
+
+        try {
+          yield rabbit.publish()
+          expect.fail("publish didn't throw an error")
+        } catch (err) {
+          expect(err).to.be.an('Error')
+          expect(err.name).to.be.eql('Test error')
+        }
+      })
+
+      after(() => {
+        sinon.restore()
+      })
+    })
+
+    context('when neither exchange, routing key or queue are provided', () => {
+      it('should throw error', function*() {
+        const rabbit = new RabbitBroker({ validate: false })
+
+        sinon.stub(rabbit, '_getPublisherChannel').resolves()
+
+        try {
+          yield rabbit.publish()
+          expect.fail("publish didn't throw an error")
+        } catch (err) {
+          expect(err).to.be.an('Error')
+          expect(err.message).to.be.eql(
+            'no exchange & routingKey specified or a simple queue'
+          )
+        }
+      })
+    })
+
+    context('when exchange and routing key are provided', () => {
+      const publishIn = { exchange: 'anExchange', routingKey: 'aRoutingKey' }
+      const message = { my: 'mesage' }
+      const executionId = '123'
+      const tryCount = 0
+      let result
+
+      let channelPublishFake
+      let getPublisherChannelStub
+
+      before(function*() {
+        const rabbit = new RabbitBroker({
+          validate: false,
+          publishIn
+        })
+
+        channelPublishFake = sinon.fake()
+
+        getPublisherChannelStub = sinon
+          .stub(rabbit, '_getPublisherChannel')
+          .resolves({ publish: channelPublishFake })
+
+        result = yield rabbit.publish(message, executionId, tryCount)
+      })
+
+      it('should return true', () => {
+        expect(result).to.be.true
+      })
+
+      it('should call _getPublisherChannel', () => {
+        expect(getPublisherChannelStub.calledOnce).to.be.true
+      })
+
+      it('should call publish', () => {
+        expect(channelPublishFake.calledOnce).to.be.true
+      })
+
+      it('should call publish with the correct parameters', () => {
+        expect(
+          channelPublishFake.calledWithMatch(
+            publishIn.exchange,
+            publishIn.routingKey,
+            sinon.match.instanceOf(Buffer),
+            {
+              headers: {
+                try_count: tryCount
+              },
+              messageId: executionId,
+              persistent: true
+            }
+          )
+        ).to.be.true
+      })
+    })
+  })
 
   context('.consume', () => {})
 
   context('.requeue', () => {})
 
   context('.stop', () => {})
+
+  context('.validate', () => {})
 })
